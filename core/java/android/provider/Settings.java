0b68b28c6f8427fdec7430817f5837eb4511781d
ezio84 <brabus84@gmail.com>
2019-11-10 14:45:35 +0100

    SystemUI: powermenu: add Screen Record toggle and make it optional (1/2)
    * Stallix edit: Adapt to 11 and make it function like https://github.com/Evolution-X/frameworks_base/commit/da9021660629604037b45c81fbbf826b9764d79b.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -9799,6 +9799,11 @@ public final class Settings {
         public static final String SCREENSHOT_IN_POWER_MENU = "screenshot_in_power_menu";
 
         /**
+         * @hide
+         */
+        public static final String SCREENRECORD_IN_POWER_MENU = "screenrecord_in_power_menu";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
